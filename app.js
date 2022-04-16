086110e0733a96a32fddfab0601bb23106977dec
Juan Manuel Alegre <alegre.juan1990@gmail.com>
2022-04-15 21:22:03 -0300

    v4

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.listen(process.env.PORT || 3000, function (){
 
 
 app.get('/',(req,res)=>{ 
-    res.sendFile(__dirname +'/views/index.html');
+    res.sendFile(__dirname +'/views/home.html');
 });
 
 app.get('/login',(req,res)=>{
